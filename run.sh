daacb45c70645d0b6376fd87c8c3f3e3358c8f77
FranciscoVogt <40207199+FranciscoVogt@users.noreply.github.com>
2026-04-18 21:45:22 +0200

    Enhance run.sh with module loading and compilation
    Added module loading and compilation steps for ghostView.

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
+#kill any switch process still active
 killall bf_switchd
 killall run_switchd
 
 
-
+#load the needed modules
 bf_kdrv_mod_load $SDE_INSTALL
 
+#compile the ghostView p4 code
 /$SDE/../tools/p4_build.sh ghostView.p4
 
 
-
+#After compilation, start the switch with the ghostView p4 code
 /$SDE/run_switchd.sh -p ghostView &
 
+#wait for the switch start
 sleep 30
 
 #Config PORTS
@@ -21,7 +24,6 @@ sleep 30
 #Config Tables, Registers etc
 /$SDE/run_bfshell.sh -b controlPlane.py 
 
-#sleep 10
 
 
 #Config PORTS
@@ -29,6 +31,6 @@ sleep 30
 
 
 
-
+#kill the process at the end
 killall bf_switchd
 killall run_switchd
